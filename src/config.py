b7b275f903acf6941dd521ac06ec07f47b99680e
martinluque12 <m.ezequiel.luque@gmail.com>
2023-07-06 00:17:02 -0300

    Archivo con algunas configuraciones (Colores, rutas, etc)

---
+++ src/config.py
@@ -0,0 +1,31 @@
+#Ancho y alto de la ventana
+WIDTH = 1200
+HEIGHT = 800
+
+#Ruta de las imagenes y los sonido
+PATH_IMAGE = "src/Recursos/Image/"
+PATH_SOUND = "src/Recursos/Sound/"
+
+#Direcciones
+DIRECTION_L = -1
+DIRECTION_R = 1
+
+#Tierra
+GROUND = 556
+
+#Colores
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
+
+#Fotogramas por segundos
+FPS = 120
+
+#Ruta de fuentes
+FONT_BREAKING = "src/Recursos/Fonts/breaking/Breaking.ttf"
+
+
+
